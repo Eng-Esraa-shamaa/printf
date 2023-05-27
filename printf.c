1ebd5ad18058887c302857ebd7b0f92eff2a1eae
Eng-Esraa-shamaa <esra2asmaibrahim@gmail.com>
2023-05-27 07:20:21 -0700

    task 0 and 1

---
+++ printf.c
@@ -0,0 +1,49 @@
+#include"main.h"
+/**
+ *_printf - function the same as printf function
+ *@format: the format that will be checked
+ *Return: the required output
+ */
+int _printf(const char *format, ...)
+{
+	int i, count = 0;
+	char *st;
+	va_list list;
+
+	if (format == NULL)
+		return (-1);
+	va_start(list, format);
+	for (i = 0; format[i] != '\0'; ++i)
+	{
+		if (format[i] != '%')
+		{
+			_putchar(format[i]), count++;
+		}
+		else
+		{
+			switch (format[i + 1])
+		{
+			case 'c':
+			_putchar(va_arg(list, int));
+			i++, count++;
+			break;
+		case 's':
+			st = va_arg(list, char *);
+			_puts(st), i++, count++;
+			break;
+		case 'd':
+case 'i':
+count += _print_int(list), i++;
+break;
+case '%':
+_putchar('%'), count++;
+break;
+default:
+count += 1;
+break;
+}
+}
+}
+via_end(list);
+return (count);
+}
