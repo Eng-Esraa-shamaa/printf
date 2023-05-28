b85e842822f032a9ac2cac3b5b21ab23064eaedd
salmaelfadil <salmaelfasdil@aucegypt.edu>
2023-05-28 09:52:53 -0700

    .

--- printf.c
+++ printf.c
@@ -18,36 +18,32 @@ int _printf(const char *format, ...)
 		if (format[i] != '%')
 			_putchar(format[i]), count++;
 
-		else if (format[i + 1] == 'c' || format[i + 1] == 's' 
-				|| format[i + 1] == 'd' || format[i + 1] == 'i' 
-				|| format[i + 1] == 'u')
-		{
-				switch (format[i + 1])
-				{
-					case 'c':
-						_putchar(va_arg(list, int)), i++, count++;
-						break;
-					case 's':
-						st = va_arg(list, char *);
-						_puts(st), i++, count++;
-						break;
-					case 'd':
-					case 'i':
-						count += _print_int(list), i++;
-						break;
-					case '%':
-						_putchar('%'), count++, i++;
-						break;
-					case 'u':
-						count += print_unsigned(list);
-						break;
-					default:
-						count++;
-						break;
+		else {
+			switch (format[i + 1])
+			{
+				case 'c':
+					_putchar(va_arg(list, int)), i++, count++;
+					break;
+				case 's':
+					st = va_arg(list, char *);
+					_puts(st), i++, count++;
+					break;
+				case 'd':
+				case 'i':
+					count += _print_int(list), i++;
+					break;
+				case '%':
+					_putchar('%'), count++, i++;
+					break;
+				case 'u':
+					count += print_unsigned(list);
+					break;
+				default:
+					_putchar(format[i]), count++;
+					break;
 				}
 		}
-		else
-			_putchar(format[i]), count++;
+	}
 	va_end(list);
 	return (count);
 }
