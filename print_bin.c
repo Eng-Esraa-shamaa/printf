6a86f3525bd5ed6b7be988664b5f3f31c942f9db
Eng-Esraa-shamaa <esra2asmaibrahim@gmail.com>
2023-05-28 08:47:59 -0700

    betty fixed

--- print_bin.c
+++ print_bin.c
@@ -1,33 +1,31 @@
 #include "main.h"
 /**
  *print_binary - function used to print binary
- *@num: the number to be printed in binary
+ *@list: the number to be printed in binary
  *Return: zero
  */
 #include "main.h"
 int print_binary(va_list list)
 {
-        unsigned int n, i;
-        unsigned int sum = 0;
-        unsigned y = 2147483648;
-        unsigned int b[32];
-        int count = 0;
+unsigned int n, i, sum = 0, y = 2147483648;
+unsigned int b[32];
+int count = 0;
 
-        n = va_arg(list, unsigned int);
-        b[0] = n / y;
-        for (i = 1; i < 32; i++)
-        {
-                y =y / 2;
-                b[i] = (n / y) % 2;
-        }
-        for (i = 0; i < 32; i++)
+n = va_arg(list, unsigned int);
+b[0] = n / y;
+for (i = 1; i < 32; i++)
 {
-                sum = sum + b[i];
-                if (sum || i == 31)
-                {
-                        _putchar(b[i] + '0');
-                        count++;
-                }
-        }
-        return (count);
+y = y / 2;
+b[i] = (n / y) % 2;
+}
+for (i = 0; i < 32; i++)
+{
+sum = sum + b[i];
+if (sum || i == 31)
+{
+_putchar(b[i] + '0');
+count++;
+}
+}
+return (count);
 }
