338ae674f94fa183ac15ba776f482baf753d380a
salmaelfadil <salmaelfasdil@aucegypt.edu>
2023-05-29 15:42:15 -0700

    .

--- printf.c
+++ printf.c
@@ -49,7 +49,7 @@ int print_helper(const char *format, specs funcs[], va_list list)
 		if (format[i] == '%')
 		{
 			if (format[i + 1] == '\0')
-				return (1);
+				return (-1);
 
 			for (; funcs[j].sym != NULL; j++)
 			{
