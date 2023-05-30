789f5952cda84f9ba581a2dfadb0b1987bac6468
Eng-Esraa-shamaa <esra2asmaibrahim@gmail.com>
2023-05-30 12:05:56 -0700

    committ

--- printf_helper.c
+++ printf_helper.c
@@ -9,42 +9,41 @@
 int print_helper(const char *format, specs funcs[], va_list list)
 {
 	int i = 0, j = 0, count = 0;
-	bool flag = false;
+bool flag = false;
 
-	if (format[i] == '\0')
-		return (-1);
-
-	while (format[i])
-	{
-		if (format[i] == '%' && format[i + 1] != '\0')
-		{
-			if (format[i + 1] == '%')
-			{
-				count += _putchar('%');
-				i += 2;
-				continue;
-			}
-			for (j = 0; funcs[j].sym != NULL; j++)
-			{
-				if (format[i + 1] == funcs[j].sym[0])
-				{
-					count += funcs[j].f(list);
-					flag = true;
-					break;
-				}
-			}
-			if (flag == false)
-			{
-				count += _putchar('%');
-				count += _putchar(format[i + 1]);
-			}
-			i += 2;
-		}
-		else
-		{
-			count += _putchar(format[i]);
-			i++;
-		}
-	}
-	return (count);
+if (format[i] == '\0')
+return (-1);
+while (format[i])
+{
+if (format[i] == '%' && format[i + 1] != '\0')
+{
+if (format[i + 1] == '%')
+{
+count += _putchar('%');
+i += 2;
+continue;
+}
+for (j = 0; funcs[j].sym != NULL; j++)
+{
+if (format[i + 1] == funcs[j].sym[0])
+{
+count += funcs[j].f(list);
+flag = true;
+break;
+}
+}
+if (flag == false)
+{
+count += _putchar('%');
+count += _putchar(format[i + 1]);
+}
+i += 2;
+}
+else
+{
+count += _putchar(format[i]);
+i++;
+}
+}
+return (count);
 }
